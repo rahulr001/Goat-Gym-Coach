f7d4385c4b0208d08c486a679c5d3fd364d24e2e
rahulr001 <rahulsquads@gmail.com>
2024-06-04 23:42:15 +0530

    added steaming response

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from app.generator import generator
+import time
 import streamlit as st
+from app.generator import generator
+from langchain_core.messages import AIMessage, HumanMessage
 
 
 st.set_page_config(page_title='Gym Coach')
@@ -8,10 +10,40 @@ st.text(body='Hi! My name is Goat your personalized AI Gym Coach, confused about
 st.text(body='no tension I got your back! Just let me know which muscle your planing to hit today!')
 st.text('')
 
-input=st.text_input("Enter the Muscle name for which your planing to hit today!: ",key="muscle_name")
-submit=st.button("Get workout")
+input = st.chat_input(
+    "Enter the Muscle name for which your planing to hit today!: ")
+
+
+def string_generator(s):
+    for char in s:
+        time.sleep(.01)
+        yield char
+
+
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
+
+st.session_state.chat_history.append(
+    AIMessage('Hi! I am your Workout Coach. How can I help you?'))
+
+with st.chat_message('AI'):
+    st.write_stream(string_generator(
+        'Hi! I am your Workout Coach. How can I help you?'))
+
+
+for message in st.session_state.chat_history:
+    if isinstance(message, HumanMessage):
+        with st.chat_message('Human'):
+            st.write(message.content)
+    else:
+        with st.chat_message('AI'):
+            st.write(message.content)
 
 if input:
-    response = generator.invoke({'question': input})
-    st.subheader("Here's your Answer")
-    st.write(response)
+    response = generator.stream({'question': input})
+    with st.chat_message('Human'):
+        st.write(input)
+        st.session_state.chat_history.append(HumanMessage(content=input))
+    with st.chat_message("AI"):
+        ans = st.write_stream(response)
+        st.session_state.chat_history.append(AIMessage(content=ans))
